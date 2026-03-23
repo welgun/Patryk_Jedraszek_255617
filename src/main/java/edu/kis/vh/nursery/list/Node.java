e52edf81bdbbe5acfdae0a3c5d9721a801f8dcb7
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-03-23 13:11:44 +0100

    11. Hermetyzacja nieprywatnych atrybutow

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -3,10 +3,35 @@ package edu.kis.vh.nursery.list;
 public class Node {
 
     private int value;
-    private Node prev, next;
+    private Node prev;
+	private Node next;
 
     public Node(int i) {
-        value = i;
+        setValue(i);
     }
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 }

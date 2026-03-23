965042972a8447d46ea6739e882b282a455f7fb5
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-03-23 13:02:58 +0100

    9. (Poprawa)Dodanie private w klasue Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,8 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public int value;
-    public Node prev, next;
+    private int value;
+    private Node prev, next;
 
     public Node(int i) {
         value = i;

bd4f2a9dd1bcdec09e1184e9eec6422b614804fd
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-03-29 14:36:27 +0200

    11. Hermetyzacja nieprywatnych atrybutow

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -11,9 +11,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -28,14 +28,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return RETURN_EMPTY;
-        return last.value;
+        return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
             return RETURN_EMPTY;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
 }
--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -9,4 +9,24 @@ public class Node {
         value = i;
     }
 
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Node getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node prev) {
+		this.prev = prev;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
 }

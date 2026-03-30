f95c141b003235a1930f4ee4b51fc0d689c50c90
Patryk Jędraszek <jedraszekpatryk@onet.pl>
2026-03-30 12:50:29 +0200

    15. Sprawdzenie testow jednostkowych

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class RhymersJUnitTest {
-
+	// Projekt jest poprawny.
     @Test
     public void testCountIn() {
         DefaultCountingOutRhymer rhymer = new DefaultCountingOutRhymer();
